7b624feaed5303dbf4bc7ef8dd5d0ac22d51317e
pablocazorla <pablo.david.cazorla@gmail.com>
2016-01-24 14:01:05 -0300

    bez class

--- js/bezier.js
+++ js/bezier.js
@@ -1,7 +1,7 @@
-var bezier = (function() {
+var Bezier = (function() {
 	"use strict";
-
-	var coord = function(x, y) {
+	var U = {
+		coord: function(x, y) {
 			var xval = x || 0,
 				yval = y || xval;
 			return {
@@ -9,68 +9,76 @@ var bezier = (function() {
 				y: yval
 			};
 		},
-		P1 = coord(),
-		P2 = coord(1),
-		C1 = coord(.3),
-		C2 = coord(.7),
-		scl = 1,
-		B = function(t, P1, P2, C1, C2) {
-			return P1 * (t * t * t) + C1 * (3 * t * t * (1 - t)) + C2 * (3 * t * (1 - t) * (1 - t)) + P2 * ((1 - t) * (1 - t) * (1 - t));
+		extend: function() {
+			var dest = {},
+				ext = function(destination, source) {
+					var source = source || {};
+					for (var property in source) {
+						if (source[property] && source[property].constructor && source[property].constructor === Object) {
+							destination[property] = destination[property] || {};
+							arguments.callee(destination[property], source[property]);
+						} else {
+							destination[property] = source[property];
+						}
+					}
+					return destination;
+				};
+			for (i = 0; i < arguments.length; i++) {
+				dest = ext(dest, arguments[i]);
+			}
+			return dest;
 		},
-		bz = function(t) {
-			var x = scl * B(t, P1.x, P2.x, C1.x, C2.x),
-				y = scl * B(t, P1.y, P2.y, C1.y, C2.y);
-			return coord(x, y);
-		};
-
-	bz.coord = coord;
-	bz.scale = function(v) {
-		if (v) {
-			scl = v;
-			return bz;
-		} else {
-			return scl;
-		}
-	};
-	bz.points = function(p1, p2, c1, c2) {
-		P1 = p1;
-		P2 = p2;
-		C1 = c1;
-		C2 = c2;
-		return bz;
-	};
-	bz.p1 = function(p) {
-		if (p) {
-			P1 = p;
-			return bz;
-		} else {
-			return P1;
-		}
-	};
-	bz.p2 = function(p) {
-		if (p) {
-			P2 = p;
-			return bz;
-		} else {
-			return P2;
-		}
-	};
-	bz.c1 = function(p) {
-		if (p) {
-			C1 = p;
-			return bz;
-		} else {
-			return C1;
-		}
-	};
-	bz.c2 = function(p) {
-		if (p) {
-			C2 = p;
-			return bz;
-		} else {
-			return C2;
+		bezier: function(t, p1, p2, c1, c2) {
+			return p1 * (t * t * t) + c1 * (3 * t * t * (1 - t)) + c2 * (3 * t * (1 - t) * (1 - t)) + p2 * ((1 - t) * (1 - t) * (1 - t));
 		}
 	};
 
-	return bz;
+	var bez = function(options) {
+
+		return this.init(options);
+	};
+	bez.prototype = {
+		init: function(options) {
+			this.config = U.extend({
+				p1: U.coord(0),
+				p2: U.coord(1),
+				c1: U.coord(0.3, 0),
+				c2: U.coord(0.7, 1)
+			}, options);
+
+
+			return this;
+		},
+		point: function(t) {
+			var t = t || 0,
+				x = U.bezier(t, this.config.p1.x, this.config.p2.x, this.config.c1.x, this.config.c2.x),
+				y = U.bezier(t, this.config.p1.y, this.config.p2.y, this.config.c1.y, this.config.c2.y);
+			return U.coord(x, y);
+		},
+		pUpdate: function(pname, p) {
+			if (p) {
+				this.config[pname].x = p.x;
+				this.config[pname].y = p.y;
+				return this;
+			} else {
+				return this.config[pname];
+			}
+		},
+		p1: function(p) {
+			return this.pUpdate('p1', p);
+		},
+		p2: function(p) {
+			return this.pUpdate('p2', p);
+		},
+		c1: function(p) {
+			return this.pUpdate('c1', p);
+		},
+		c2: function(p) {
+			return this.pUpdate('c2', p);
+		}
+	};
+
+	return function(options) {
+		return new bez(options);
+	};
 })();
